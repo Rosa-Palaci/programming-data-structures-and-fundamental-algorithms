9fffb829c2f821e8b3670c8fb8e6a2b406d7413d
Rosa Palaci <palacibelrose431@gmail.com>
2022-08-09 10:32:03 -0500

    Add instructions

---
+++ act-1.cpp
@@ -0,0 +1,8 @@
+/* Actividad 1.1 - Funciones Iterativas, Recursivas y su análisis de Complejidad
+Rosa Vanessa Palacios Beltran A01652612
+TC1031.632*/
+
+// Instrucciones: Calculé la sumatorioa de 1 hasta n y llame a tres funciones:
+// sumalterativa: Calcule la sumatoria de 1 hasta n conn un metodo iterativo, con una entrada de un entero positivo (n), salida la sumatoria de 1 hasta n, precondición n es un entrero positivo y postcondición ninguna
+// sumaRecursiva: Calcule la sumatoria de 1 hasta n con un metodo recursivo, con una entrada de un entero positivo (n), salida la sumatoria de 1 hasta n, precondición n es un entero positivo, postcondición nada.
+// sumaDirecta: Calcule la sumatoria de 1 hasta n con un metodo matemático directo, con una entrada de un entero positivo (n), salida la sumatoria de 1 hasta n, precondición n es un entero positivo, postcondición nada.
